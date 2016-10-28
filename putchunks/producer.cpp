2c6391a9b81be1c89d888993782787d623317283
alhowaidi <malhowaidi@gmail.com>
2016-10-28 15:20:51 -0500

    update produce

--- putchunks/producer.cpp
+++ putchunks/producer.cpp
@@ -67,7 +67,7 @@ std::cerr << "Interest: " << interest << std::endl;
   std::cerr << "interest name: " << name << std::endl;
   const auto s=name[1];
 
-  const std::string ss = interest.getName()[1].toUri();
+  const std::string ss = interest.getName()[1].toUri() +"/" +interest.getName()[2].toUri();
 //  std::cerr << "File name: " << ss << std::endl;
  // std::cerr << "store size: " << m_store.size() << std::endl;
   
@@ -75,50 +75,57 @@ std::cerr << "Interest: " << interest << std::endl;
      //std::ifstream& is;
      std::cerr << "File name: " << ss << std::endl;
      m_prefix = name;
-     m_versionedPrefix = Name(m_prefix).appendVersion();
+     m_versionedPrefix = Name(m_prefix);//.appendVersion();
      populateStore(ss);
 
   }
   
   shared_ptr<Data> data;
-//std::cerr << "-----" << std::endl;
-//std::cerr << "name.size()" << name.size() << std::endl;
-//std::cerr << "m_versionedPre.size() " << m_versionedPrefix.size() << std::endl;
-//std::cerr << "isPrefixOfName: " <<m_versionedPrefix.isPrefixOf(name) << std::endl;
-//std::cerr << "isSegment: " << name[-1].isSegment() << std::endl; 
 
-  //std::cerr << "is segment: " << name[-1].isSegment() << std::endl;
+  bool lastSegment = false;
+
   // is this a discovery Interest or a sequence retrieval?
   if (name.size() == m_versionedPrefix.size() + 1 && m_versionedPrefix.isPrefixOf(name) &&
       name[-1].isSegment()) {
     const auto segmentNo = static_cast<size_t>(interest.getName()[-1].toSegment());
     // specific segment retrieval
     
-    
+    std::cerr << "in if"  << std::endl;
     if (segmentNo < m_store.size()) {
       data = m_store[segmentNo];
       std::cerr << "sno: " << segmentNo << std::endl;
   //    std::cerr << "*data: " << *data << std::endl;
+
+      if(segmentNo == m_store.size() -1 )
+    	  lastSegment = true;
     }
-//    else{
-  //    std::cerr << "sno: " << segmentNo << std::endl;
-    //}
+
   }
   else if (interest.matchesData(*m_store[0])) {
     // Interest has version and is looking for the first segment or has no version
     data = m_store[0];
-    //std::cerr << "in else if"  << std::endl;
+    std::cerr << "in else if"  << std::endl;
+    if(m_store.size() == 1)
+    	lastSegment = true;
    // std::cerr << data << std::endl;
     //std::cerr << "mstore[0]: "<< *m_store[0] << std::endl;
   }
-  else{
+//  else{
 //    std::cerr << "in else!!" << std::endl;
-  }
+//     std::cerr << "*mstore[0]: "<< *m_store[0] << std::endl;
+//     std::cerr << "mstore[0]: "<< m_store[0] << std::endl;
+//  }
 
   if (data != nullptr) {
     if (m_isVerbose)
       std::cerr << "Data: " << *data << std::endl;
 
+    if(lastSegment == true)
+    {
+    	std::cerr << "clear store .. " << std::endl;
+    	m_store.clear();
+
+    }
     m_face.put(*data);
   }
 //else
@@ -142,7 +149,7 @@ Producer::populateStore(std::string fileName)
     const auto nCharsRead = is.gcount();
     if (nCharsRead > 0) {
       auto data = make_shared<Data>(Name(m_versionedPrefix).appendSegment(m_store.size()));
-//      std::cerr << "data: " << data << std::endl;
+
       data->setFreshnessPeriod(m_freshnessPeriod);
       data->setContent(&buffer[0], nCharsRead);
 
